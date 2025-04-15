f029f33cca9396c2658c54461f6841c2d728ce81
predator27 <anand.naman997@gmail.com>
2025-04-15 18:07:50 +0530

    first and last occurrence - leetcode medium

--- day08-o1/firatAndLastOccurrence.py
+++ day08-o1/firatAndLastOccurrence.py
@@ -1,5 +1,8 @@
 ## ~~~~~~~~ first and last occurrence ~~~~~~~~~ 
 
+
+## https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+
 ## given an sorted array of elements, find the first and last occurrence oof the target element 
 
 
@@ -18,9 +21,7 @@ def findOccurrences(arr, target):
         else:
             start = mid + 1 
 
-
-
-
+    ## to find second occurrences 
     start = 0 
     end = len(arr) -1 
     while start <= end:
@@ -38,6 +39,8 @@ def findOccurrences(arr, target):
     return first, second 
     
 
+## time - O(logn) + o(logn) = o(logn)
+## space - o(1)
 
 
 arr = [12,23,24,56,76,76,76,79]
@@ -46,3 +49,28 @@ arr = [12,23,24,56,76,76,76,79]
 print(findOccurrences(arr, 76)) ## (4,6)
 
 print(findOccurrences(arr, 79)) ## (7, 7)
+
+
+
+## BRUTE force Approach - linear search 
+
+
+
+def findFirstAndLast(arr, target):
+    first, last = -1, -1 
+    for i in range(len(arr)):
+        if arr[i] == target:
+            if first == -1:
+                first = i 
+            last = i 
+
+    return [first,last]
+
+## time - o(n) : as we scan evrry elems once 
+## space - o(1)
+
+arr1 = [12,23,24,56,76,76,76,79, 80, 80]
+arr = [12,23,24,56,76,76,76,79, 81]
+print(findFirstAndLast(arr1, 80)) ## (8,9)
+
+print(findFirstAndLast(arr, 81)) ## (8,8)
---
+++ day08-o1/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+print(findOccurrences(arr, 76)) ## (4,6)
+
+print(findOccurrences(arr, 79)) ## (7, 7)
