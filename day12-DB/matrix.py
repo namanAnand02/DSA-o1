d8bd15608c5b00d4b384606d142efc722acc91c0
predator27 <anand.naman997@gmail.com>
2025-04-20 13:43:40 +0530

    addition and sub of two matrices

--- day12-DB/matrix.py
+++ day12-DB/matrix.py
@@ -76,41 +76,101 @@
 
 ## ~~~~~~~ 2. sum of all elements in the matrix ~~~~~~~~~~~~~~
 
-def sumOfAllElements(arr, m,n):
-    ## m = rows no 
-    ## n = cols no 
+# def sumOfAllElements(arr, m,n):
+#     ## m = rows no 
+#     ## n = cols no 
 
-    ## for m,n = 3,3 - arr = [[1,2,3], [4,5,6], [7,8,9]]
-    ## arr[0] = [1,2,3]
-    ## arr[1] = [4,5,6]
-    ## arr[2] = [7,8,9]
+#     ## for m,n = 3,3 - arr = [[1,2,3], [4,5,6], [7,8,9]]
+#     ## arr[0] = [1,2,3]
+#     ## arr[1] = [4,5,6]
+#     ## arr[2] = [7,8,9]
 
-    ## sum(arr[0]) = 6 
-    ## sum(arr[1]) = 15 
-    ## sum(arr[2]) = 24 
+#     ## sum(arr[0]) = 6 
+#     ## sum(arr[1]) = 15 
+#     ## sum(arr[2]) = 24 
 
-    ## finalSum = 6 + 15 + 24 = 45 
+#     ## finalSum = 6 + 15 + 24 = 45 
 
-    sumV = 0 
-    for i in range(m):
-        sumV += sum(arr[i])
+#     sumV = 0 
+#     for i in range(m):
+#         sumV += sum(arr[i])
         
 
 
-    return sumV
+#     return sumV
 
 
-m = int(input("enter the no of rows: "))
-n = int(input("enter the no of cols: "))
-arr = []
-for i in range(m):
-    ## arr += [int(i) for i in input().split()] ## WRONG  ## only enter inputs n times that too space separated 
+# m = int(input("enter the no of rows: "))
+# n = int(input("enter the no of cols: "))
+# arr = []
+# for i in range(m):
+#     ## arr += [int(i) for i in input().split()] ## WRONG  ## only enter inputs n times that too space separated 
 
-    arr.append([int(i) for i in input().split()]) ##This ensures we're appending a list (a row) into your 2D matrix.
-
-
-
-print(sumOfAllElements(arr, m, n)) ## 45 
+#     arr.append([int(i) for i in input().split()]) ##This ensures we're appending a list (a row) into your 2D matrix.
 
 
 
+# print(sumOfAllElements(arr, m, n)) ## 45 
+
+
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+## ~~~~~~~~~~~~~ 3. addition of two matrices ~~~~~~~~~~~~
+
+def additionOfMatrices(L1, L2, m,n):
+    L3 = []
+    for i in range(m):
+        row = [] ## create a new row to store sums of current row. We’re preparing one row at a time.
+        ##  Inside that, loop through each column (from 0 to n-1) for that row.
+        for j in range(n):
+            row.append(L1[i][j] + L2[i][j])
+
+        L3.append(row)
+
+
+    return L3 
+
+m = 3 
+n = 3 
+L1 = [[1,2,3], [4,5,6], [7,8,9]]
+L2 = [[2,2,2], [1,1,1], [2,1,0]]
+
+
+# print(additionOfMatrices(L1,L2,m,n))
+## [[3, 4, 5], [5, 6, 7], [9, 9, 9]]
+
+
+
+
+
+
+
+
+
+def subtractionOfTwoMatrices(L1, L2, m,n):
+    L3 = []
+    for i in range(m):
+        row = [] ## create a new row to store sums of current row. We’re preparing one row at a time.
+        ##  Inside that, loop through each column (from 0 to n-1) for that row.
+        for j in range(n):
+            row.append(L1[i][j] - L2[i][j])
+
+        L3.append(row)
+
+
+    return L3 
+
+m = 3 
+n = 3 
+L1 = [[1,2,3], [4,5,6], [7,8,9]]
+L2 = [[2,2,2], [1,1,1], [2,1,0]]
+
+
+
+print(subtractionOfTwoMatrices(L1, L2, m, n))
+## [[-1, 0, 1], [3, 4, 5], [5, 7, 9]]
