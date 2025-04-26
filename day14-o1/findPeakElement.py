cd15887df943d78d438e2b3b69c9577dc24c4184
predator27 <anand.naman997@gmail.com>
2025-04-26 13:09:43 +0530

    follow up to prev ques- find peak

--- day14-o1/findPeakElement.py
+++ day14-o1/findPeakElement.py
@@ -1,4 +1,6 @@
-## ~~~~~~~~~~ Finde peak element ~~~~~~~~~~~~~~~~
+## ~~~~~~~~~~ Find peak element ~~~~~~~~~~~~~~~~
+
+## leetcode 162: medium level problem 
 
 ## all distinct elements in the array 
 
@@ -69,13 +71,53 @@ def findPeaK_brute(nums):
 
 ## time - o(n) 
 
-print(findPeaK_brute([10])) ## 0
-print(findPeaK_brute([10,12])) ## 1 
-print(findPeaK_brute([10,12, 11, 9, 13, 8])) ## 1 
-print(findPeaK_brute([10,11,13, 19, 23, 24])) ## 5
+# print(findPeaK_brute([10])) ## 0
+# print(findPeaK_brute([10,12])) ## 1 
+# print(findPeaK_brute([10,12, 11, 9, 13, 8])) ## 1 
+# print(findPeaK_brute([10,11,13, 19, 23, 24])) ## 5
 
 
 ## in case the question asks us to return all the peaks -- we can do that as well with this approach 
 ## we can store all the peaks in a separate data structure as we move along the array and find them and then finally return that data structure as the final answer. 
         
     
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~ follow up question ~~~~~~~~~~~~~~~~
+
+
+## what if the ques asks us to return the max peak element in the array 
+
+
+# arr = [1,2,3,4,5,4,3,2] ## return the index value of element 5 i.e index 4 
+
+def findPeaK_brute(nums):
+    if len(nums) == 1:
+        return 0
+    maxPeak = float("-inf")
+    for i in range(len(nums)):
+        if (i == 0 and nums[i] > nums[i +1 ]):
+            maxPeak = max(maxPeak, nums[i])
+
+        elif (i == len(nums)-1 and nums[i] > nums[i -1 ]):
+            maxPeak = max(maxPeak, nums[i])
+             
+        
+        ### else:
+        ###     if (nums[i] > nums[i + 1] and nums[i] > nums[i -1 ]):
+        ###         maxPeak = max(maxPeak, nums[i])
+
+        ## we will have to run this else case when elems are not at 0 or at len(nums) -1 
+        else:
+            if i!= 0 and i!= len(nums) -1:
+                if nums[i] > nums[i -1 ] and nums[i] > nums[ i + 1]:
+                    maxPeak = max(maxPeak,nums[i])
+
+
+    return nums.index(maxPeak) 
+
+## time - o(n)
+
+print(findPeaK_brute([10]))  ## 0
+print(findPeaK_brute([10,12])) ## 1
+print(findPeaK_brute([10,12, 11, 9, 13, 8]))  ## 4
+print(findPeaK_brute([10,11,13, 19, 23, 24])) ## 5
